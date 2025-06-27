af65b3091e56c7c35d68774a3ac3862823e03a9f
lucasoliveirads <89103765+lucasoliveirads@users.noreply.github.com>
2025-06-26 21:28:21 -0300

    Update scan.js

--- pages/api/scan.js
+++ pages/api/scan.js
@@ -1,1 +1,16 @@
+export default async function handler(req, res) {
+  const { ip } = req.query;
+  const SHODAN_API_KEY = process.env.SHODAN_API_KEY;
 
+  if (!ip) {
+    return res.status(400).json({ error: 'IP não informado' });
+  }
+
+  try {
+    const response = await fetch(`https://api.shodan.io/shodan/host/${ip}?key=${SHODAN_API_KEY}`);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: 'Erro ao consultar Shodan', details: err.message });
+  }
+}
